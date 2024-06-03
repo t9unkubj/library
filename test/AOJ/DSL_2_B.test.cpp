c376814d558f23e14c83e69fa4e494c0168561e5
t9unkubj <149492297+t9unkubj@users.noreply.github.com>
2024-06-03 23:48:36 +0900

    Create DSL_2_B.test.cpp

---
+++ test/AOJ/DSL_2_B.test.cpp
@@ -0,0 +1,12 @@
+#include"template.hpp"
+#include"segtree.hpp"
+int main(){
+    INT(n,q);
+    segtree<ll,op,0ll>seg(n);
+    while(q--){
+        INT(t,x,y);
+        --x;
+        if(t==0)seg.set(x,seg.get(x)+y);
+        else PRT(seg.prod(x,y));
+    }
+}
