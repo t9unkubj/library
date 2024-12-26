d6ac44143724e2ea6dd753f7b02868e78bb5a943
t9unkubj <149492297+t9unkubj@users.noreply.github.com>
2024-12-26 15:01:42 +0900

    Update affine.hpp

--- data_structure/monoid/affine.hpp
+++ data_structure/monoid/affine.hpp
@@ -3,10 +3,10 @@ struct monoid{
     T a,b;
     monoid()=default;
     monoid(T a,T b):a(a),b(b){}
-    static monoid op(T a,T b){
-        return monoid(a.a*b.a,a.b*b.a+b.b);
+    static monoid<T> op(monoid<T> a,monoid<T> b){
+        return monoid<T>(a.a*b.a,a.b*b.a+b.b);
     }
-    static monoid e(){
+    static monoid<T> e(){
         return monoid(1,0);
     }
 };
